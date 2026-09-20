4e191ef41ece3180a377dca2bd0bd24ca6d88378
partho800 <parthoomadder@gmail.com>
2026-09-20 14:31:25 +0600

    chore: remove unused screens, empty cart folder, and legacy placeholder components

--- src/components/cart/index.ts
+++
@@ -1,2 +0,0 @@
-// Cart components export
-export {};
--- src/screens/Product/ProductScreen.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ProductDetailView } from '../../components/product/ProductDetailView';
-
-export const ProductScreen: React.FC = () => {
-  return <ProductDetailView />;
-};
-
